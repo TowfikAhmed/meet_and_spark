3289157b309d1bf67fae9288f8fe6a726c1d8d91
shimul <shimul929@gmail.com>
2025-10-15 23:46:46 +0600

    f

---
+++ app/page.tsx
@@ -0,0 +1,101 @@
+'use client';
+
+import { useRouter, useSearchParams } from 'next/navigation';
+import React, { Suspense, useState } from 'react';
+import { encodePassphrase, generateRoomId, randomString } from '@/lib/client-utils';
+import styles from '../styles/Home.module.css';
+
+function Tabs(props: React.PropsWithChildren<{}>) {
+  const searchParams = useSearchParams();
+  const tabIndex = searchParams?.get('tab') === 'custom' ? 1 : 0;
+
+  const router = useRouter();
+  function onTabSelected(index: number) {
+    const tab = index === 1 ? 'custom' : 'demo';
+    router.push(`/?tab=${tab}`);
+  }
+
+  let tabs = React.Children.map(props.children, (child, index) => {
+    return (
+      <button
+        className="lk-button"
+        onClick={() => {
+          if (onTabSelected) {
+            onTabSelected(index);
+          }
+        }}
+        aria-pressed={tabIndex === index}
+      >
+        {/* @ts-ignore */}
+        {child?.props.label}
+      </button>
+    );
+  });
+
+  return (
+    <div className={styles.tabContainer}>
+      <div className={styles.tabSelect}>{tabs}</div>
+      {/* @ts-ignore */}
+      {props.children[tabIndex]}
+    </div>
+  );
+}
+
+function DemoMeetingTab(props: { label: string }) {
+  const router = useRouter();
+  const [e2ee, setE2ee] = useState(false);
+  const [sharedPassphrase, setSharedPassphrase] = useState(randomString(64));
+  const startMeeting = () => {
+    if (e2ee) {
+      router.push(`/rooms/${generateRoomId()}#${encodePassphrase(sharedPassphrase)}`);
+    } else {
+      router.push(`/rooms/${generateRoomId()}`);
+    }
+  };
+  return (
+    <div className={styles.tabContent}>
+      <p style={{ margin: 0 }}>Make it easy, beautiful, enjoyable.</p>
+      <button style={{ marginTop: '1rem' }} className="lk-button" onClick={startMeeting}>
+        Start Meeting
+      </button>
+      <div style={{ display: 'flex', flexDirection: 'column', gap: '1rem' }}>
+        {/* <div style={{ display: 'flex', flexDirection: 'row', gap: '1rem' }}>
+          <input
+            id="use-e2ee"
+            type="checkbox"
+            checked={e2ee}
+            onChange={(ev) => setE2ee(ev.target.checked)}
+          ></input>
+          <label htmlFor="use-e2ee">Enable end-to-end encryption</label>
+        </div> */}
+        {e2ee && (
+          <div style={{ display: 'flex', flexDirection: 'row', gap: '1rem' }}>
+            <label htmlFor="passphrase">Passphrase</label>
+            <input
+              id="passphrase"
+              type="password"
+              value={sharedPassphrase}
+              onChange={(ev) => setSharedPassphrase(ev.target.value)}
+            />
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default function Page() {
+  return (
+    <>
+      <main className={styles.main} data-lk-theme="default">
+        <div className="header">
+          <img src="/images/logo.png" alt="LiveKit Meet" width="160" />
+          <h2>Spark Video Conferencing.</h2>
+        </div>
+        <Suspense fallback="Loading">
+          <DemoMeetingTab label="Demo" />
+        </Suspense>
+      </main>
+    </>
+  );
+}
