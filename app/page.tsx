9aaa653384dae4443598d1331d0db493df299e2e
shimul <shimul929@gmail.com>
2025-11-29 22:53:14 +0600

    dfgujhg

--- app/page.tsx
+++ app/page.tsx
@@ -13,23 +13,11 @@ export default async function Page() {
 
   return (
     <>
-      <img
-        className=" w-screen absolute z-[-100] top-0 left-0 object-contain opacity-20"
-        src="/images/bg_cover.svg"
-        alt="MEET logo"
-      />
       <main
         data-lk-theme="default"
         className="relative z-1 w-full max-w-6xl mx-auto h-screen flex flex-col md:flex-row items-center justify-center"
       >
-        <section className="relative flex-1 flex flex-col items-center justify-center  gap-6">
-          <img className="h-16" src="/images/logo.gif" alt="MEET logo" />
-          <h2 className="text-dim">Business & Social Video Conferencing.</h2>
-          <PageMeetingOnboarding />
-        </section>
-        <div className="flex-1">
-          <PageClientAuthCard url={url} anonKey={anonKey} />
-        </div>
+        aaaaaaa
       </main>
     </>
   );
